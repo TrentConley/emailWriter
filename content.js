4895a2311cab2e5db1b93ebd3c373a778bfa133e
Trent Conley <trentconley@gmail.com>
2022-12-21 19:00:02 -0600

    Eliminated chrome.tobs dependency

--- content.js
+++ content.js
@@ -1,27 +1,7 @@
-console.log("Test");
-
-function pause(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function delayedAction() {
-  console.log("Starting action");
-  for (let i = 0; i < 5; i++) {
-    await pause(1000); // Pause for 1 second
-  }
-  console.log("Action complete");
-}
-
-delayedAction();
-
 // Wait for the page to finish loading
-// Set up an event handler for the chrome.tabs.onUpdated event
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  // Check if the URL of the tab is the Gmail compose page
-  // if (tab.url && tab.url.includes("https://mail.google.com/mail/u/0/#compose")) {
-  console.log("Hello World!");
+window.addEventListener("load", function () {
   // Find the textarea element where the user is composing their email
-  var textarea = document.querySelector("textarea[name=to]");
+  var textarea = document.querySelector("textarea[aria-label='Message Body']");
 
   // Set up a mutation observer to detect when the text in the textarea changes
   var observer = new MutationObserver(function (mutations) {
@@ -29,17 +9,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       // When the text changes, retrieve the current text from the textarea
       var text = textarea.value;
 
-      // Do something with the text (e.g. send it to the background script for processing)
-      chrome.runtime.sendMessage(
-        {
-          type: "processText",
-          text: text,
-        },
-        function (response) {
-          // When the background script responds, modify the text in the textarea
-          textarea.value = response.text;
-        }
-      );
+      // Check if the text includes the word "hello"
+      if (text.includes("hello")) {
+        // Replace the word "hello" with "zork" in the textarea
+        textarea.value = text.replace("hello", "zork");
+      }
     });
   });
 
