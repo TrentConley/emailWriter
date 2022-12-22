d9f12d44ea2ff5af33b19284a1370b057f204455
Trent Conley <trentconley@gmail.com>
2022-12-21 19:11:32 -0600

    trying fix from chatGPT

--- content.js
+++ content.js
@@ -1,10 +1,38 @@
 // Wait for the page to finish loading
 window.addEventListener("load", function () {
-  console.log("Test");
-
   // Find the textarea element where the user is composing their email
   var textarea = document.querySelector("textarea[aria-label='Message Body']");
 
+  // If the textarea element is not found, set up a mutation observer to listen for when it is added to the page
+  if (!textarea) {
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        // Check if the textarea element has been added to the page
+        var addedNodes = Array.from(mutation.addedNodes);
+        var textarea = addedNodes.find(function (node) {
+          return node.tagName === "TEXTAREA";
+        });
+
+        // If the textarea element has been added, set up the MutationObserver to listen for changes to the text
+        if (textarea) {
+          observer.disconnect();
+          setUpObserver(textarea);
+        }
+      });
+    });
+
+    // Start observing the page for changes to the DOM
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  } else {
+    // If the textarea element is found, set up the MutationObserver to listen for changes to the text
+    setUpObserver(textarea);
+  }
+});
+
+function setUpObserver(textarea) {
   // Set up a mutation observer to detect when the text in the textarea changes
   var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
@@ -17,7 +45,6 @@ window.addEventListener("load", function () {
         textarea.value = text.replace("hello", "zork");
       }
     });
-    System.out.println();
   });
 
   // Start observing the textarea for changes
@@ -25,4 +52,4 @@ window.addEventListener("load", function () {
     characterData: true,
     subtree: true,
   });
-});
+}
